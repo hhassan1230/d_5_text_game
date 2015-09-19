e24287b723a275e159811fa1f76d3605f5c46f2e
hhassan1230 <hhassan@oswego.edu>
2015-09-19 01:02:54 -0400

    starts json file

--- javascript/main.js
+++ javascript/main.js
@@ -2,9 +2,28 @@ $("document").ready(function(){
   
  
     $("button").on("click", function(){
-    	debugger;
       var newScene = "<h1>Start of Game</h1>";
-      $('.scene').html(newScene);
+    	var choice = $(this).attr('class');
+			switch (choice) { 
+				
+				case 'maybeStart': 
+					var maybe = "<h1>We'll also take that as a yes...</h1>";
+
+					$('.scene').html(maybe);
+		    	// debugger;
+		    	window.setTimeout($('.scene').html(newScene), 200000);
+					// $('.scene').delay( 80000 ).html(newScene);
+					break;
+				case 'dontStart': 
+					var no = "<h1>We'll take that as a yes...</h1>";
+
+					$('.scene').html(no);
+					// $('.scene').delay( 80000 ).html(newScene);
+					break;
+				default:
+					$('.scene').html(newScene);
+			}
+      
     });
   
 })

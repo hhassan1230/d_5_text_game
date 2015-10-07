cf4cd4f370963b5599af587b6fced42687f39008
hhassan1230 <hhassan@oswego.edu>
2015-10-06 21:15:11 -0400

    adds buttons functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,48 @@
 $("document").ready(function(){
   // var text = ;
-  var obj = {};
+  var obj = {},
+  whereAmI = "";
   $.getJSON("./javascript/text.json", function(json) {
-		// debugger;
     var obj = json;
-    $("button").on("click", function(){
+    $(".yeah button, .nah button, .maybe button").on("click", function(){
       var newScene = "<h1>" + obj.start["text"] + "</h1>";
+      var currentChoice = "<section class='choice'>"
+      for (var i = 0; i < obj.start["choice"].length; i++) {
+				var key = Object.keys(obj.start["choice"][i]);
+				currentChoice += "<button class='"+ key +"'>"+ obj.start["choice"][i][key] +"</button> ";	
+      };
+      currentChoice += "</section>";
     	var choice = $(this).attr('class');
 			switch (choice) { 
 				case 'maybeStart': 
 					var maybe = "<h1>We'll also take that as a yes...</h1>";
-
 					$('.scene').html(maybe);
-		    	// debugger;
 		    	setTimeout(function() {
-						$('.scene').html(newScene);
+						$('.scene').html(newScene + currentChoice);
 					}, 2000);
-					// $('.scene').delay( 80000 ).html(newScene);
 					break;
 				case 'dontStart': 
 					var no = "<h1>We'll take that as a yes...</h1>";
 					$('.scene').html(no);
 					
 					setTimeout(function() {
-						$('.scene').html(newScene);
+						$('.scene').html(newScene + currentChoice);
 					}, 2000);
-					// $('.scene').delay(8000),.html(no)//.delay( 8000 ).html(newScene);
-					// debugger;
-
-					// $('.scene').html(newScene);
 					break;
 				default:
-					$('.scene').html(newScene);
+					$('.scene').html(newScene + currentChoice);
 			}
+			var newStoryJson = {};
+
+			// obj
+			// for (var key in obj) {
+  	// 			debugger;
+			// }
       
     });
+		$("button").on("click", function(){
+			debugger;
+		});
 	});
   
 })
